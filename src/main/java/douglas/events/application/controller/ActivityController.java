eed9fea850d63b1920006c42a7c836897ba7f695
Wesley00s <wesley300rodrigues@gmail.com>
2025-08-13 19:08:00 -0300

    refactor: refactor endpoints in activity controller

--- src/main/java/douglas/events/application/controller/ActivityController.java
+++ src/main/java/douglas/events/application/controller/ActivityController.java
@@ -1,13 +1,13 @@
 package douglas.events.application.controller;
 
 import douglas.events.application.dto.ActivityDto;
+import douglas.events.application.dto.response.ApiResponse;
 import douglas.events.business.service.ActivityService;
 import douglas.events.infraestructure.model.Activity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/activity")
@@ -17,22 +17,29 @@ public class ActivityController {
 
     @PostMapping("/create-activity/{eventId}")
     public ResponseEntity<String> createActivity(@PathVariable Long eventId, @RequestBody ActivityDto activityDto) {
-        var newActivity = activityService.createActivity(eventId, activityDto);
 
+        var newActivity = activityService.createActivity(eventId, ActivityDto.toEntity(activityDto));
         return ResponseEntity.ok().body("Atividade criada com sucesso para o evento " + newActivity.getEvent().getName());
     }
 
     @GetMapping("/get-all-activities/{eventId}")
-    public ResponseEntity<List<Activity>> getAllActivities(@PathVariable Long eventId) {
-        var listActivities = activityService.findAllActivitiesByEvent(eventId);
+    public ResponseEntity<ApiResponse<Activity>> getAllActivities(
+            @PathVariable Long eventId,
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
+    ) {
+        var listActivities = activityService.findAllActivitiesByEvent(eventId, page, pageSize);
 
-        return ResponseEntity.ok().body(listActivities);
+        return activityService.getApiResponseEntity(listActivities);
     }
 
     @GetMapping("/get-all-activities-by-event")
-    public ResponseEntity<List<ActivityDto>> getAllActivitiesByActiveEvent() {
-        var listActivities = activityService.findAllActivitiesByActiveEvent();
+    public ResponseEntity<ApiResponse<Activity>> getAllActivitiesByActiveEvent(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
+    ) {
+        var listActivities = activityService.findAllActivitiesByActiveEvent(page, pageSize);
 
-        return ResponseEntity.ok().body(listActivities);
+        return activityService.getApiResponseEntity(listActivities);
     }
 }
