9c70ca9cef89f9674f02823ab44879b3d16c647d
Wesley00s <wesley300rodrigues@gmail.com>
2025-08-15 14:23:10 -0300

    refactor: refactor participant service

--- src/main/java/douglas/events/business/service/ParticipantService.java
+++ src/main/java/douglas/events/business/service/ParticipantService.java
@@ -6,12 +6,15 @@ import douglas.events.infraestructure.exception.local.EnrollmentException;
 import douglas.events.infraestructure.exception.local.EventNotFoundException;
 import douglas.events.infraestructure.exception.local.ListEmptyException;
 import douglas.events.infraestructure.exception.local.NotFoundException;
-import douglas.events.infraestructure.model.Participant;
+import douglas.events.infraestructure.model.Person;
+import douglas.events.infraestructure.model.enums.Role;
 import douglas.events.infraestructure.repository.EventRepository;
-import douglas.events.infraestructure.repository.ParticipantRepository;
+import douglas.events.infraestructure.repository.PersonRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,59 +24,54 @@ import java.util.List;
 public class ParticipantService {
 
     private final EnrollmentService enrollmentService;
-    private final ParticipantRepository participantRepository;
+    private final PersonRepository personRepository;
     private final EventRepository eventRepository;
     private final ApplicationEventPublisher eventPublisher;
 
 
     @Transactional
-    public Participant createParticipant(Long eventId, Participant participant) {
+    public Person createParticipant(Long eventId, Person participant) {
         if (!eventRepository.existsById(eventId)) {
             throw new EventNotFoundException("Evento não encontrado com o ID: " + eventId);
         }
-
-        var savedParticipant = participantRepository.save(participant);
-
+        participant.setRole(Role.PARTICIPANT);
+        var savedParticipant = personRepository.save(participant);
         var enrollment = enrollmentService.createEnrollment(eventId, savedParticipant.getId());
 
         if (enrollment == null) {
             throw new EnrollmentException("Não foi possível criar a inscrição para o participante.");
         }
-
         this.eventPublisher.publishEvent(new ParticipantCreatedEventDTO(enrollment));
-
         return savedParticipant;
     }
 
-    public List<ParticipantDto> getAllParticipants() {
-        var participants = participantRepository.findAll();
+    public Page<Person> getAllParticipants(Integer page, Integer pageSize) {
+        var participants = personRepository.findByRole(Role.PARTICIPANT, PageRequest.of(page, pageSize));
 
         if (participants.isEmpty()) {
             throw new ListEmptyException("Não há participantes inscritos.");
         }
 
-        return participants.stream()
-                .map(ParticipantDto::fromEntity)
-                .toList();
+        return participants;
+    }
+
+    public Person getParticipantByEmail(String email) {
+        var participant = personRepository.findByEmail(email);
+
+        if (participant == null) {
+            throw new NotFoundException("Participante não encontrado.");
+        }
+
+        return participant;
     }
 
     public ParticipantDto getParticipantById(Long participantId) {
-        var participant = participantRepository.findById(participantId);
+        var participant = personRepository.findById(participantId);
 
         if (participant.isEmpty()) {
             throw new NotFoundException("Participante não encontrado.");
         }
 
         return ParticipantDto.fromEntity(participant.get());
-    }
-
-    public Participant getParticipantEntityById(Long participantId) {
-        var participant = participantRepository.findById(participantId);
-
-        if (participant.isEmpty()) {
-            throw new NotFoundException("Participante não encontrado.");
-        }
-
-        return participant.get();
     }
 }
