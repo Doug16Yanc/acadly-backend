0703ad09b308ef26480c25e0ab810ee3673ea875
Wesley00s <wesley300rodrigues@gmail.com>
2025-08-15 14:20:48 -0300

    update: update auth service

--- src/main/java/douglas/events/business/service/AuthService.java
+++ src/main/java/douglas/events/business/service/AuthService.java
@@ -1,12 +1,16 @@
 package douglas.events.business.service;
 
 import douglas.events.application.dto.request.AuthAdminResponseDto;
-import douglas.events.application.dto.CreateAdminDto;
+import douglas.events.application.dto.request.AuthEmployeeResponseDto;
+import douglas.events.application.dto.request.CreateAdminDto;
 import douglas.events.infraestructure.config.security.TokenService;
-import douglas.events.infraestructure.exception.authentication.AuthAdminNotMatchesException;
-import douglas.events.infraestructure.exception.local.NotFoundException;
-import douglas.events.infraestructure.model.Administrator;
-import douglas.events.infraestructure.repository.AdministratorRepository;
+import douglas.events.infraestructure.exception.authentication.AuthNotMatchesException;
+import douglas.events.infraestructure.exception.local.AdminNotFoundException;
+import douglas.events.infraestructure.exception.local.EmployeeNotFoundException;
+import douglas.events.infraestructure.exception.local.UsernameAlreadyExistsException;
+import douglas.events.infraestructure.model.Person;
+import douglas.events.infraestructure.model.enums.Role;
+import douglas.events.infraestructure.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,40 +18,68 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final AdministratorRepository administratorRepository;
+    private final PersonRepository personRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
 
-    public Administrator createAdmin(CreateAdminDto createAdminDto) {
-        var alreadyExistent = administratorRepository.findByUsername(createAdminDto.username());
+    public Person createAdmin(CreateAdminDto createAdminDto) {
+        var alreadyExistent = personRepository.findByEmail(createAdminDto.email());
 
         if (alreadyExistent != null) {
-            throw new RuntimeException("Username already exists");
+            throw new UsernameAlreadyExistsException("Email já cadastrado.");
         }
 
-        Administrator admin = new Administrator();
-        admin.setUsername(createAdminDto.username());
+        var admin = new Person();
+
+        admin.setName(createAdminDto.name());
+        admin.setEmail(createAdminDto.email());
         admin.setPassword(passwordEncoder.encode(createAdminDto.password()));
+        admin.setRole(Role.ADMIN);
 
-        Administrator savedAdmin = administratorRepository.save(admin);
-
-        String jwtToken = tokenService.generateToken(savedAdmin.getUsername());
+        var savedAdmin = personRepository.save(admin);
+        tokenService.generateToken(savedAdmin.getUsername());
 
         return savedAdmin;
     }
 
-    public Administrator findAdminByUsername(String username) {
-        var admin = administratorRepository.findByUsername(username);
-        if (admin == null) throw new NotFoundException("Administrador não encontrado.");
+    public Person findAdminByUsername(String email) {
+        var admin = personRepository.findByEmail(email);
+        if (admin == null) throw new AdminNotFoundException("Administrador não encontrado.");
+
+        if (admin.getRole() != Role.ADMIN) {
+            throw new AdminNotFoundException("Administrador não encontrado.");
+        }
         return admin;
     }
 
-    public AuthAdminResponseDto authenticateAdmin(String username, String rawPassword) {
-        var admin = findAdminByUsername(username);
+    public AuthAdminResponseDto authenticateAdmin(String email, String rawPassword) {
+        var admin = findAdminByUsername(email);
         if (!passwordEncoder.matches(rawPassword, admin.getPassword())) {
-            throw new AuthAdminNotMatchesException();
+            throw new AuthNotMatchesException();
         }
+
         var token = tokenService.generateToken(admin.getUsername());
         return new AuthAdminResponseDto(token, admin.getId(), admin.getUsername());
     }
+
+    public Person findEmployeeByUsername(String email) {
+        var employee = personRepository.findByEmail(email);
+        if (employee == null) throw new EmployeeNotFoundException("Funcionário não encontrado com o email: " + email);
+
+        if (employee.getRole() != Role.EMPLOYEE) {
+            throw new EmployeeNotFoundException("Funcionário não encontrado com o email: " + email);
+        }
+        return employee;
+    }
+
+    public AuthEmployeeResponseDto authenticateEmployee(String email, String rawPassword) {
+        var employee = findEmployeeByUsername(email);
+        if (!passwordEncoder.matches(rawPassword, employee.getPassword())) {
+            throw new AuthNotMatchesException();
+        }
+
+        var token = tokenService.generateToken(employee.getUsername());
+        return new AuthEmployeeResponseDto(token, employee.getId(), employee.getUsername());
+    }
+
 }
