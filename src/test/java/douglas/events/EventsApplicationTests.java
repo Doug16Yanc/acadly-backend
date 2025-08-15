3117a4896eeca4ff64ae3131c8c1fd1c3b6fc223
Wesley00s <wesley300rodrigues@gmail.com>
2025-08-15 15:37:56 -0300

    test: implement test containers

--- src/test/java/douglas/events/EventsApplicationTests.java
+++ src/test/java/douglas/events/EventsApplicationTests.java
@@ -3,8 +3,14 @@ package douglas.events;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class EventsApplicationTests {
 
     static {
@@ -13,6 +19,17 @@ class EventsApplicationTests {
                 System.setProperty(entry.getKey(), entry.getValue())
         );
     }
+
+    @Container
+    static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+
 
     @Test
     void contextLoads() {
