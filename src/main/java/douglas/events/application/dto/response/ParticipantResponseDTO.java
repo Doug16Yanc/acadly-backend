12495a7cd91daaecd8ac4d52178de6e6146b27ee
Wesley00s <wesley300rodrigues@gmail.com>
2025-08-15 14:20:08 -0300

    update: update response dtos

--- src/main/java/douglas/events/application/dto/response/ParticipantResponseDTO.java
+++ src/main/java/douglas/events/application/dto/response/ParticipantResponseDTO.java
@@ -1,6 +1,6 @@
 package douglas.events.application.dto.response;
 
-import douglas.events.infraestructure.model.Participant;
+import douglas.events.infraestructure.model.Person;
 import douglas.events.infraestructure.model.enums.ParticipantType;
 
 public record ParticipantResponseDTO(
@@ -9,7 +9,7 @@ public record ParticipantResponseDTO(
         String email,
         ParticipantType participantType
 ) {
-    public static ParticipantResponseDTO fromEntity(Participant participant) {
+    public static ParticipantResponseDTO fromEntity(Person participant) {
         return new ParticipantResponseDTO(
                 participant.getId(),
                 participant.getName(),
